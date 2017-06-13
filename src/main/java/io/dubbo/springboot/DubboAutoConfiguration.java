686c019f2096937d3b1253e68f9e6364bcd2ceca
leoops <214508914@qq.com>
2017-06-13 17:15:29 +0800

    123123

--- src/main/java/io/dubbo/springboot/DubboAutoConfiguration.java
+++ src/main/java/io/dubbo/springboot/DubboAutoConfiguration.java
@@ -17,79 +17,79 @@ import org.springframework.context.annotation.Configuration;
 @EnableConfigurationProperties(DubboProperties.class)
 public class DubboAutoConfiguration {
 
-    @Autowired
-    private DubboProperties dubboProperties;
+	@Autowired
+	private DubboProperties dubboProperties;
 
-    @Bean
-    public ApplicationConfig requestApplicationConfig() {
-    	ApplicationConfig applicationConfig = dubboProperties.getApplication();
-    	if(applicationConfig == null){
-    		applicationConfig = new ApplicationConfig();
-    	}
-        return applicationConfig;
-    }
+	@Bean
+	public ApplicationConfig requestApplicationConfig() {
+		ApplicationConfig applicationConfig = dubboProperties.getApplication();
+		if (applicationConfig == null) {
+			applicationConfig = new ApplicationConfig();
+		}
+		return applicationConfig;
+	}
 
-    @Bean
-    public RegistryConfig requestRegistryConfig() {
-    	RegistryConfig registryConfig = dubboProperties.getRegistry();
-    	if(registryConfig == null){
-    		registryConfig = new RegistryConfig();
-    	}
-        return registryConfig;
-    }
+	@Bean
+	public RegistryConfig requestRegistryConfig() {
+		RegistryConfig registryConfig = dubboProperties.getRegistry();
+		if (registryConfig == null) {
+			registryConfig = new RegistryConfig();
+		}
+		return registryConfig;
+	}
 
-    @Bean
-    public ProtocolConfig requestProtocolConfig() {
-    	ProtocolConfig protocolConfig = dubboProperties.getProtocol();
-    	if(protocolConfig == null){
-    		protocolConfig = new ProtocolConfig();
-    	}
-        return protocolConfig;
-    }
+	@Bean
+	public ProtocolConfig requestProtocolConfig() {
+		ProtocolConfig protocolConfig = dubboProperties.getProtocol();
+		if (protocolConfig == null) {
+			protocolConfig = new ProtocolConfig();
+		}
+		return protocolConfig;
+	}
 
-    @Bean
-    public MonitorConfig requestMonitorConfig() {
-    	MonitorConfig monitorConfig = dubboProperties.getMonitor();
-    	if(monitorConfig == null){
-    		monitorConfig = new MonitorConfig();
-    	}
-        return monitorConfig;
-    }
-    
-    @Bean
-    public ProviderConfig requestProviderConfig(){
-    	ProviderConfig providerConfig = dubboProperties.getProvider();
-    	if(providerConfig == null){
-    		providerConfig = new ProviderConfig();
-    	}
-    	return providerConfig;
-    }
-    
-    @Bean
-    public ModuleConfig requestModuleConfig(){
-    	ModuleConfig moduleConfig = dubboProperties.getModule();
-    	if(moduleConfig == null){
-    		moduleConfig = new ModuleConfig();
-    	}
-    	return moduleConfig;
-    }
-    
-    @Bean
-    public MethodConfig requestMethodConfig(){
-    	MethodConfig  methodConfig = dubboProperties.getMethod();
-    	if(methodConfig == null){
-    		methodConfig = new MethodConfig();
-    	}
-    	return methodConfig;
-    }
-    
-    @Bean
-    public ConsumerConfig requestConsumerConfig(){
-    	ConsumerConfig consumerConfig = dubboProperties.getConsumer();
-    	if(consumerConfig == null){
-    		consumerConfig = new ConsumerConfig();
-    	}
-    	return consumerConfig;
-    }
+	@Bean
+	public MonitorConfig requestMonitorConfig() {
+		MonitorConfig monitorConfig = dubboProperties.getMonitor();
+		if (monitorConfig == null) {
+			monitorConfig = new MonitorConfig();
+		}
+		return monitorConfig;
+	}
+
+	@Bean
+	public ProviderConfig requestProviderConfig() {
+		ProviderConfig providerConfig = dubboProperties.getProvider();
+		if (providerConfig == null) {
+			providerConfig = new ProviderConfig();
+		}
+		return providerConfig;
+	}
+
+	@Bean
+	public ModuleConfig requestModuleConfig() {
+		ModuleConfig moduleConfig = dubboProperties.getModule();
+		if (moduleConfig == null) {
+			moduleConfig = new ModuleConfig();
+		}
+		return moduleConfig;
+	}
+
+	@Bean
+	public MethodConfig requestMethodConfig() {
+		MethodConfig methodConfig = dubboProperties.getMethod();
+		if (methodConfig == null) {
+			methodConfig = new MethodConfig();
+		}
+		return methodConfig;
+	}
+
+	@Bean
+	public ConsumerConfig requestConsumerConfig() {
+		ConsumerConfig consumerConfig = dubboProperties.getConsumer();
+		if (consumerConfig == null) {
+			consumerConfig = new ConsumerConfig();
+		}
+		return consumerConfig;
+	}
 
 }
